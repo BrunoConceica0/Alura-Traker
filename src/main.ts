e9ec2535ca72696167a6d093a8b0cac7dfd1318a
Bruno <bd687353@gmail.com>
2025-07-25 14:27:52 -0300

    fix/Correção no main.ts, erro de função library.add()

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
+// FontAwesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faStop, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faStop, faPlay);
+// Adicionando ícones individuais à biblioteca
+library.add(faStop as any, faPlay as any);
 
+// Registrando o componente global
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
