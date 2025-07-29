4600b80bb39eb277ee7b2c0f963d84c25ec9640e
Bruno <bd687353@gmail.com>
2025-07-29 13:49:18 -0300

    feat/Funcionalidade para alterar o projecto e criar um novo

--- src/components/SelectorProject.vue
+++ src/components/SelectorProject.vue
@@ -3,7 +3,7 @@
     <select
       :value="idProject"
       @change="updateProject(($event.target as HTMLSelectElement).value)"
-      class="w-full px-4 py-2 border-2 border-blue-500 rounded-md text-gray-700 text-base focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-600 bg-white appearance-none cursor-pointer"
+      class="selector-container"
     >
       <option value="">Selecione um projeto</option>
       <option v-for="project in projects" :key="project.id" :value="project.id">
--- src/router/views/Projects.vue
+++ src/router/views/Projects.vue
@@ -1,57 +1,35 @@
 <template>
-  <section class="container p-[1.25rem]">
+  <section class="container p-[1.25rem] project-container">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salveProject">
-      <div class="mb-4 relative">
-        <label for="nomeProject" id="nomeProject" class="project-label">
-          Nome do Projeto
-        </label>
-        <input
-          type="text"
-          id="nomeProject"
-          v-model="nameProject"
-          placeholder="Digite seu projeto"
-          class="project-input"
-        />
-      </div>
-      <div class="mb-4 relative">
-        <button type="submit" class="btnBox">Salva</button>
-      </div>
-    </form>
+    <router-link :to="{ path: '/projects/new' }" class="projects-link">
+      <span> <font-awesome-icon icon="fa-plus" /></span>
+      <span>Novo Projeto</span>
+    </router-link>
+    <Form />
     <Table :listProjects="listProjects" />
   </section>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+import { computed, defineComponent } from "vue";
 import Table from "../../components/Table.vue";
+import Form from "../../router/views/Projects/Form.vue";
 import { useStore } from "../../store/index";
+
 export default defineComponent({
   name: "Projects",
-  components: { Table },
+  components: { Table, Form },
 
   setup() {
-    const nameProject = ref("");
+    const store = useStore();
     // Lista de projetos
 
-    const store = useStore();
-
-    const salveProject = () => {
-      if (!nameProject.value.trim()) {
-        alert("O nome do projeto não pode estar vazio!");
-        return;
-      } else {
-        store.commit("ADD_PROJECT", nameProject.value);
-        nameProject.value = "";
-      }
-    };
+    const listProjects = computed(() => {
+      return store.state.projects;
+    });
 
     return {
-      nameProject,
-      salveProject,
-      store,
-      // A lista de projetos sem do IProdutos que é tipado com mutante vuex, usando o store.commit("ADD_PROJECT", nameProject.value);
-      listProjects: computed(() => store.state.projects),
+      listProjects,
     };
   },
 });
