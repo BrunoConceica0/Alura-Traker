7f359f9574784298ea0b0aa84b402486db536a0c
Bruno <bd687353@gmail.com>
2025-07-29 14:18:35 -0300

    refactor/Criando rotas filhas do projeto

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,21 +6,29 @@ const routes = [
     name: "Tasks",
     component: () => import("./views/Tasks.vue"),
   },
+
   {
     path: "/projects",
     name: "Projects",
     component: () => import("./views/Projects.vue"),
-  },
-  {
-    path: "/projects/new",
-    name: "New project",
-    component: () => import("./views/Projects/Form.vue"),
-  },
-  {
-    path: "/projects/:id",
-    name: "Edit project",
-    props: true,
-    component: () => import("./views/Projects/Form.vue"),
+    children: [
+      {
+        path: "",
+        name: "Project",
+        component: () => import("./views/Projects/List.vue"),
+      },
+      {
+        path: "new",
+        name: "New project",
+        component: () => import("./views/Projects/Form.vue"),
+      },
+      {
+        path: "/:id",
+        name: "Edit project",
+        props: true,
+        component: () => import("./views/Projects/Form.vue"),
+      },
+    ],
   },
 ];
 
