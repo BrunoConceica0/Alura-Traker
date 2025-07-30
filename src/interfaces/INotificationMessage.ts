864deba945612afa03ca4ae91bcad779e8954913
Bruno <bd687353@gmail.com>
2025-07-30 14:41:58 -0300

    fix: Arrumando bug para build

--- src/interfaces/INotificationMessage.ts
+++ src/interfaces/INotificationMessage.ts
@@ -1,11 +1,13 @@
-export enum typeNotification {
-  SUCCESS,
-  FAILURE,
-  ATTENTION,
-}
+export const typeNotification = {
+  SUCCESS: "SUCCESS",
+  FAILURE: "FAILURE",
+  ATTENTION: "ATTENTION",
+} as const;
 export default interface Notification {
   title: string;
   text: string;
   type: typeNotification;
   id: number;
 }
+export type typeNotification =
+  (typeof typeNotification)[keyof typeof typeNotification];
